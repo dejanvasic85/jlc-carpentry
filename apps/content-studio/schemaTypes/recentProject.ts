0d79bd1e71b6ba188b8a4ad54fa25457c4da32a4
Dejan Vasic <dejanvasic24@gmail.com>
2025-08-11 21:03:48 +1000

    Renaming the content item for recent projects

--- apps/content-studio/schemaTypes/recentProject.ts
+++ apps/content-studio/schemaTypes/recentProject.ts
@@ -1,8 +1,8 @@
 import { defineField, defineType } from 'sanity';
 
 export default defineType({
-  name: 'recentProject',
-  title: 'Recent Project',
+  name: 'recentProjects',
+  title: 'Recent Projects',
   type: 'document',
   fields: [
     defineField({
