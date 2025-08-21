53afd61add7661a0e371096f12176ffd25a2494e
Dejan Vasic <dejanvasic24@gmail.com>
2025-08-21 22:39:58 +1000

    Fixing the sanity config

--- apps/content-studio/sanity.config.ts
+++ apps/content-studio/sanity.config.ts
@@ -6,7 +6,7 @@ import { schemaTypes } from './schemaTypes';
 export default defineConfig({
   name: 'default',
   title: 'jlc-carpentry',
-  projectId: 'o0G2VIEZ1',
+  projectId: '365wnpgg',
   dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
